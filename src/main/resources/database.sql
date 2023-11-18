c7bd62f046b2a4e7e6daae516af555ddd4141380
ZhoomartSas <zhoomart.0507@gmail.com>
2023-11-18 14:07:30 +0600

    COM13

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,13 +1,13 @@
 INSERT INTO bank  (bank_name,logo_url
 )
 VALUES('RSK','g'),
-      ('MBank','g');
+      ('MBank','g'),
+('KISB','g');
 INSERT INTO credit_type(type_cred)
 VALUES ('Потребительские'),
                 ('Ипотека');
 INSERT INTO  tariff
     (max_credit_amount,min_credit_amount,max_credit_term,min_credit_term,
      interest_rate,bank_id,credit_type_id) VALUES (250000,5000,18,3,29,1,1),
-                                                  (150000,5000,18,3,19,1,1),
-                                                  (200000,5000,18,3,23,1,1),
-                                                  (250000,5000,18,3,30,1,1);
+                                                  (200000,5000,18,3,23,2,1),
+                                                  (250000,5000,18,3,30,3,1);
